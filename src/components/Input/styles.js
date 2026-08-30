761c1f2f8016577ff8533fe8aa86f315cd87f0dc
Richard Grenet <EoknDev@gmail.com>
2026-08-30 05:44:52 -0400

    created new base in vite, transferred over simple non-functional assets

--- src/components/Input/styles.js
+++
@@ -1,9 +0,0 @@
-import { makeStyles } from '@material-ui/core/styles';
-
-export default makeStyles((theme) => ({
-    iconToggle: {
-    [theme.breakpoints.down('300')]: {
-        display: 'none',
-    },
-    },
-}));
